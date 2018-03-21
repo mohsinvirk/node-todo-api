2b0f29f982a74acdaf2d0db287710e452755d3a8
Mohsin Latif <mohsinlatifvirk@gmail.com>
2018-03-21 17:13:54 +0500

    heroku

--- server/server.js
+++ server/server.js
@@ -1,55 +1,55 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./model/todo');
+const { Users } = require('./model/users');
 
-var app = express();
-
+const app = express();
+const port= process.env.PORT;
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  var todo = new Todo({
-    text: req.body.text
-  });
+    const todo = new Todo({
+        text: req.body.text
+    });
 
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    })
 });
 
 app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
-    res.send({todos});
-  }, (e) => {
-    res.status(400).send(e);
-  });
+    Todo.find().then((todos) => {
+        res.send(todos);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
-app.get('/todos/:id', (req, res) => {
-  var id = req.params.id;
+app.get('todos/:id', (req, res) => {
+    const id = req.params.id;
 
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
-
-  Todo.findById(id).then((todo) => {
-    if (!todo) {
-      return res.status(404).send();
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
     }
+    Todo.findById(id).then((todo) => {
+        if (!toodo) {
+            return res.status(404).send();
 
-    res.send({todo});
-  }).catch((e) => {
+        }
+        res.send({ todo });
+    })
+
+}).catch((e) => {
     res.status(400).send();
-  });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
-});
 
-module.exports = {app};
+
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
+});
